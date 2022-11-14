074d4014633b587f385aba84eba5983a9b4b8890
Gleb Dmitriev <woodrunsdeep@gmail.com>
2022-11-14 21:05:05 +0400

    fix: eleventy config update

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,6 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/img");
-	eleventyConfig.addPassthroughCopy("./src/css");
-	eleventyConfig.addWatchTarget("./src/css");
+	eleventyConfig.addWatchTarget("./src/css/style.css");
   	return {
     	dir: {
     		input: "src",
